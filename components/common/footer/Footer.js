788639dda3280cb001ffaae8d3438b07a71792f8
luuciluu <luuci.luu27@gmail.com>
2017-02-24 11:48:56 +0100

    Added structure to the folder

---
+++ components/common/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+           <footer>
+               <h1>Esto es el footer</h1>
+           </footer>
+        );
+    }
+}
